6b3c31a859384790a63d07fd441b30e90d42410f
ho3einHamidi <hosseinmhh@yahoo.com>
2024-04-16 10:48:01 +0330

    feat: header section finished

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+import contactIcon from "../../Icons/contact-icon.png";
+import createIcon from "../../Icons/create-icon.svg";
+export function Header() {
+  return (
+    <div className="header">
+      <img className="contact-icon" src={contactIcon} alt="contact-icon"></img>
+      <p>Contacts</p>
+      <img className="create-icon" src={createIcon} alt="create-icon"></img>
+    </div>
+  );
+}
