8676530efcdc572427a53b3259e04768c9702b4d
ho3einHamidi <hosseinmhh@yahoo.com>
2024-04-17 09:25:04 +0330

    feat: clear search

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,14 +1,27 @@
 import "./searchBar.css";
 import searchIcon from "../../Icons/search-icon.png";
-export function SearchBar({value,onChange}) {
+import cancelIcon from "../../Icons/cancel-icon.png";
+export function SearchBar({ value, onChange }) {
+  const onClear = ()=>{
+    onChange("")
+  }
   return (
     <div className="search-bar">
       <div className="wrapper">
         <div className="img-wrapper">
-
-        <img src={searchIcon} className="search-icon" alt="search-icon"></img>
+          <img src={searchIcon} className="search-icon" alt="search-icon"></img>
         </div>
-        <input value={value} onChange={(e) => onChange(e.target.value)} className="input" type="text" placeholder="Search"></input>
+        <input
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          className="input"
+          type="text"
+          placeholder="Search"
+        ></input>
+        {value && <div className="cancel-button-wrapper">
+          <img onClick={onClear} className="cancel-icon" src={cancelIcon} alt="cancel-icon"></img>
+        </div>}
+        
       </div>
     </div>
   );
